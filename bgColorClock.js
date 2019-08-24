5b89eb65d712bfffaddf8c959041dd302b59d4de
Luke Bowman <lukethomasbowman@gmail.com>
2019-08-23 22:18:18 -0700

    Fixed bgColorClock.js and made it external.

---
+++ bgColorClock.js
@@ -0,0 +1,82 @@
+function updateBackground() {
+  var body = document.body,
+    bstyle = body.style,
+    clock = document.querySelector(".clock");
+  switch (new Date().getHours()) {
+    case 0:
+      bstyle.backgroundColor = "hsl(240, 75%, 75%)";
+      break;
+    case 1:
+      bstyle.backgroundColor = "hsl(255, 75%, 75%)";
+      break;
+    case 2:
+      bstyle.backgroundColor = "hsl(270, 75%, 75%)";
+      break;
+    case 3:
+      bstyle.backgroundColor = "hsl(285, 75%, 75%)";
+      break;
+    case 4:
+      bstyle.backgroundColor = "hsl(300, 75%, 75%)";
+      break;
+    case 5:
+      bstyle.backgroundColor = "hsl(315, 75%, 75%)";
+      break;
+    case 6:
+      bstyle.backgroundColor = "hsl(330, 75%, 75%)";
+      break;
+    case 7:
+      bstyle.backgroundColor = "hsl(345, 75%, 75%)";
+      break;
+    case 8:
+      bstyle.backgroundColor = "hsl(0, 75%, 75%)";
+      break;
+    case 9:
+      bstyle.backgroundColor = "hsl(15, 75%, 75%)";
+      break;
+    case 10:
+      bstyle.backgroundColor = "hsl(30, 75%, 75%)";
+      break;
+    case 11:
+      bstyle.backgroundColor = "hsl(45, 75%, 75%)";
+      break;
+    case 12:
+      bstyle.backgroundColor = "hsl(60, 75%, 75%)";
+      break;
+    case 13:
+      bstyle.backgroundColor = "hsl(75, 75%, 75%)";
+      break;
+    case 14:
+      bstyle.backgroundColor = "hsl(90, 75%, 75%)";
+      break;
+    case 15:
+      bstyle.backgroundColor = "hsl(105, 75%, 75%)";
+      break;
+    case 16:
+      bstyle.backgroundColor = "hsl(120, 75%, 75%)";
+      break;
+    case 17:
+      bstyle.backgroundColor = "hsl(135, 75%, 75%)";
+      break;
+    case 18:
+      bstyle.backgroundColor = "hsl(150, 75%, 75%)";
+      break;
+    case 19:
+      bstyle.backgroundColor = "hsl(165, 75%, 75%)";
+      break;
+    case 20:
+      bstyle.backgroundColor = "hsl(180, 75%, 75%)";
+      break;
+    case 21:
+      bstyle.backgroundColor = "hsl(195, 75%, 75%)";
+      break;
+    case 22:
+      bstyle.backgroundColor = "hsl(210, 75%, 75%)";
+      break;
+    case 23:
+      bstyle.backgroundColor = "hsl(225, 75%, 75%)";
+      break;
+  }
+}
+
+setInterval(updateBackground, 1000 * 60);
+updateBackground();
